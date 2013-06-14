75c3b7aea43052789eea606e390f0071255fe312
Santiago <santi.1234@gmail.com>
2013-06-14 11:55:42 -0400

    Fixed the zendesk feedback tab

---
+++ js/feedback_tab.js
@@ -0,0 +1,17 @@
+	<!-- Zendesk js for support tab -->
+<script type="text/javascript" src="//assets.zendesk.com/external/zenbox/v2.6/zenbox.js"></script>
+<style type="text/css" media="screen, projection">
+  @import url(//assets.zendesk.com/external/zenbox/v2.6/zenbox.css);
+</style>
+<script type="text/javascript">
+  if (typeof(Zenbox) !== "undefined") {
+    Zenbox.init({
+      dropboxID:   "20185603",
+      url:         "https://oxtablet.zendesk.com",
+      tabTooltip:  "Soporte",
+      tabImageURL: "https://assets.zendesk.com/external/zenbox/images/tab_es_support.png",
+      tabColor:    "black",
+      tabPosition: "Left"
+    });
+  }
+</script>
